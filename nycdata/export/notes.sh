85f200a7a191f4c7ede918d65a71775383c90a9f
Eric Brelsford <ebrelsford@gmail.com>
2015-03-02 16:42:39 -0500

    Actually escape quotes

--- nycdata/export/notes.sh
+++ nycdata/export/notes.sh
@@ -1,4 +1,4 @@
-SQL="SELECT l.bbl, '\"' || REPLACE(REPLACE(n.noter, '\"', E'\\\"'), ',', E'\\,') || '\"' AS added_by_name, n.added, '\"' || REPLACE(REPLACE(n.text, '\"', E'\"\"'), ',', E'\\,') || '\"' AS text FROM lots_lot l INNER JOIN organize_note n ON n.lot_id = l.id;"
+SQL="SELECT l.bbl, '\"' || REPLACE(REPLACE(n.noter, '\"', E'\"\"'), ',', E'\\,') || '\"' AS added_by_name, n.added, '\"' || REPLACE(REPLACE(n.text, '\"', E'\"\"'), ',', E'\\,') || '\"' AS text FROM lots_lot l INNER JOIN organize_note n ON n.lot_id = l.id;"
 
 DB_NAME=ebrelsford_fns
 DB_USER=ebrelsford_fns
