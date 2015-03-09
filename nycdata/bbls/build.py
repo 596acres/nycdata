c1226d372459579090750dcbfa433e47babe0555
Eric Brelsford <ebrelsford@gmail.com>
2015-03-09 18:04:53 -0400

    BBLs

---
+++ nycdata/bbls/build.py
@@ -0,0 +1,15 @@
+from ..boroughs import get_borough_number
+
+
+def build_bbl(borough, block, lot):
+    """
+    Build a BBL given the constituent borough, block and lot.
+
+    BBLs are unique identifiers for tax lots in New York City.
+    """
+    # Accept borough as string but convert it first
+    try:
+        borough = int(borough)
+    except ValueError:
+        borough = get_borough_number(borough)
+    return '%d%05d%04d' % (borough, block, lot)
