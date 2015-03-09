9a88c4d3d8a5f1bd2689f94ebbfe299760747424
Eric Brelsford <ebrelsford@gmail.com>
2015-03-09 18:04:42 -0400

    Get borough number

--- nycdata/boroughs/__init__.py
+++ nycdata/boroughs/__init__.py
@@ -1,2 +1,2 @@
-from .common import borough_number
+from .common import borough_number, get_borough_number
 from .find import find_borough
--- nycdata/boroughs/common.py
+++ nycdata/boroughs/common.py
@@ -5,3 +5,12 @@ borough_number = {
     4: 'Queens',
     5: 'Staten Island',
 }
+
+
+def get_borough_number(name):
+    """Try to find the borough number for the given name."""
+    name = name.lower()
+    for k, v in borough_number.items():
+        if name == v.lower():
+            return k
+    return None
